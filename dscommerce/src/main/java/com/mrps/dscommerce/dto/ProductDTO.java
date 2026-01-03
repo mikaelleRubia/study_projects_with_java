0c055c5f15762b83c644aac1da9756a99393c648
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2026-01-03 19:31:45 -0300

    feat: adding ProductDTO

---
+++ dscommerce/src/main/java/com/mrps/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,51 @@
+package com.mrps.dscommerce.dto;
+
+import com.mrps.dscommerce.entities.Product;
+
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgURL;
+
+    public ProductDTO(){
+
+    }
+    public ProductDTO( Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+        this.imgURL = product.getImgURL();
+    }
+    public ProductDTO(Long id, String name, String description, Double price, String imgURL) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgURL = imgURL;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+}
