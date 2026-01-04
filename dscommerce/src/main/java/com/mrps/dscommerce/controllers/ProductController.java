8563722e4d6c8f7b3539f508de2140ee897065c2
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2026-01-03 21:56:09 -0300

    feat: adding ResponseEntity in the controller

--- dscommerce/src/main/java/com/mrps/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/mrps/dscommerce/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.mrps.dscommerce.controllers;
 
 import com.mrps.dscommerce.dto.ProductDTO;
-import com.mrps.dscommerce.entities.Product;
 import com.mrps.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
-import java.util.Optional;
+import java.net.URI;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -18,20 +19,23 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value= "/{id}")
-    public ProductDTO findById(@PathVariable Long id){
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
         ProductDTO productDTO = service.findById(id);
-        return productDTO;
+        return ResponseEntity.ok(productDTO);
     }
 
     @GetMapping()
-    public Page<ProductDTO> findAll(Pageable page){
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable page){
         Page<ProductDTO> results = service.findAll(page);
-        return results;
+        return ResponseEntity.ok(results);
     }
 
     @PostMapping()
-    public ProductDTO insert(@RequestBody ProductDTO dto){
-
-        return service.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
+
