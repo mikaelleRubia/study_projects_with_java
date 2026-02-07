9a6234aa6125a4c286c2370ded2d390e9c87d29d
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2026-02-07 11:30:13 -0300

    feat: adding set and get for Id

--- clients/src/main/java/com/example/clients/entities/Client.java
+++ clients/src/main/java/com/example/clients/entities/Client.java
@@ -30,6 +30,13 @@ public class Client {
         this.bithDate = bithDate;
         this.children = children;
     }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
