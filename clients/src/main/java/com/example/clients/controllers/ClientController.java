be2212caaaaf14741c4d0c2e3caefb9d1f7af1fb
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2026-02-08 14:24:43 -0300

    feat: update, delete and insert in the Controller

--- clients/src/main/java/com/example/clients/controllers/ClientController.java
+++ clients/src/main/java/com/example/clients/controllers/ClientController.java
@@ -2,14 +2,15 @@ package com.example.clients.controllers;
 
 import com.example.clients.dto.ClientDTO;
 import com.example.clients.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -28,5 +29,26 @@ public class ClientController {
         Page<ClientDTO> results = service.findAll(page);
         return ResponseEntity.ok(results);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id,@Valid @RequestBody ClientDTO dto){
+        dto = service.update(dto, id);
+        return  ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 
 }
